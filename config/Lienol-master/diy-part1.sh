73087e45641947df872098f7e7900505ab28bff8
keduowei <67183405+keduowei@users.noreply.github.com>
2024-03-25 21:00:59 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -8,6 +8,8 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
