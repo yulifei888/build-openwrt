f64f2287fdeb228734290a32d38d8129acc8feee
keduowei <67183405+keduowei@users.noreply.github.com>
2024-03-28 09:28:18 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -9,7 +9,7 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
+sed -i '5d' target/linux/x86/Makefile
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
