32a9e0aa1b5f6f60403fd54e5473ab2fde2fa63c
keduowei <67183405+keduowei@users.noreply.github.com>
2024-04-05 16:52:32 +0800

    Create 1.sh

---
+++ config/lede-master/1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+#========================================================================================================================
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# selet love kenel
+# sed -i '13d' target/linux/x86/Makefile
+# sed -i '13i KERNEL_PATCHVER:=5.4' target/linux/x86/Makefile
+
+# other
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
